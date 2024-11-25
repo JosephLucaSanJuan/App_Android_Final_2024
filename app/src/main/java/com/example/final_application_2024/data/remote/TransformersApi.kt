bd99aa93977885ed7b990ebf7f6cdb64077c6c71
JosephLucaSanJuan <jurassicjoe03@gmail.com>
2024-11-25 22:49:33 +0100

    api_modifications

--- app/src/main/java/com/example/final_application_2024/data/remote/TransformersApi.kt
+++ app/src/main/java/com/example/final_application_2024/data/remote/TransformersApi.kt
@@ -2,9 +2,16 @@ package com.example.final_application_2024.data.remote
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface TransformersApi {
+    /*@POST("api/v2/transformers")
+    suspend fun create():Response<TransformersListResponse>
+
+    @GET("api/v2/transformers/{id}")
+    suspend fun update(@Path("id") id:String):TransformersListResponse*/
+
     @GET("api/v2/transformers")
     suspend fun readAll():Response<TransformersListResponse>
 
