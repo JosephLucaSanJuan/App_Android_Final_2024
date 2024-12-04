68d1cee3b7339ef3a8a4eed7b2c0237a21c90049
JosephLucaSanJuan <jurassicjoe03@gmail.com>
2024-12-04 16:40:59 +0100

    gradle_adjustments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.example.final_application_2024"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.final_application_2024"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
