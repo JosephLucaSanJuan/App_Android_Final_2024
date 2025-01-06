8b26f1a077dab8556efc7883b24beae2b25c6c1f
JosephLucaSanJuan <jurassicjoe03@gmail.com>
2025-01-06 23:07:20 +0100

    modifying_the_register

--- app/src/main/java/com/example/final_application_2024/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/example/final_application_2024/ui/register/RegisterFragment.kt
@@ -10,6 +10,7 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.final_application_2024.databinding.FragmentRegisterBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -41,14 +42,32 @@ class RegisterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.registerButton.setOnClickListener {
+            val name = binding.nameInput.text.toString()
+            val surname = binding.surnameInput.text.toString()
+            val email = binding.emailInput.text.toString()
+            val password = binding.passwordInput.text.toString()
+            viewModel.onRegister(name, surname, email, password)
+        }
         viewLifecycleOwner.lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.uiState.collect {
                     uiState -> when(uiState) {
-                        is RegisterListUiState.InitialState -> TODO()
-                        is RegisterListUiState.Error -> TODO()
-                        is RegisterListUiState.Loading -> {}
+                        is RegisterListUiState.InitialState -> {
+                            /*hideError()
+                            enableInput()*/
+                        }
+                        is RegisterListUiState.Error -> {
+                            /*enableInput()
+                            showError(uiState.message)*/
+                        }
+                        is RegisterListUiState.Loading -> {
+                            disableInput()
+                            hideError()
+                        }
                         is RegisterListUiState.Registered -> {
+                            hideError()
+                            findNavController().popBackStack()
                             binding.registerButton.setOnClickListener {
                                 val action = RegisterFragmentDirections.actionRegisterFragmentToFactionFragment()
                                 view.findNavController().navigate(action)
@@ -62,6 +81,35 @@ class RegisterFragment : Fragment() {
             val action = RegisterFragmentDirections.actionRegisterFragmentToLoginFragment()
             view.findNavController().navigate(action)
         }
+    }
+
+    private fun disableInput() {
+        binding.registerButton.isEnabled = false
+        binding.nameInput.isEnabled = false
+        binding.surnameInput.isEnabled = false
+        binding.emailInput.isEnabled = false
+        binding.passwordInput.isEnabled = false
+        binding.confirmPassword.isEnabled = false
+    }
+    private fun enableInput() {
+        binding.registerButton.isEnabled = true
+        binding.nameInput.isEnabled = false
+        binding.surnameInput.isEnabled = false
+        binding.emailInput.isEnabled = true
+        binding.passwordInput.isEnabled = true
+        binding.confirmPassword.isEnabled = true
+    }
+    private fun showError(message:String) {
+        binding.name.error = message
+        binding.surname.error = message
+        binding.email.error = message
+        binding.password.error = message
+    }
+    private fun hideError() {
+        binding.name.error = null
+        binding.surname.error = null
+        binding.email.error = null
+        binding.password.error = null
     }
 
 }
--- app/src/main/java/com/example/final_application_2024/ui/register/RegisterViewModel.kt
+++ app/src/main/java/com/example/final_application_2024/ui/register/RegisterViewModel.kt
@@ -1,23 +1,44 @@
 package com.example.final_application_2024.ui.register
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.final_application_2024.data.UserRepository
+import com.example.final_application_2024.ui.login.LoginListUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class RegisterViewModel @Inject constructor() : ViewModel() {
+class RegisterViewModel @Inject constructor(
+    private val userRepository: UserRepository
+) : ViewModel() {
     private val _uiState = MutableStateFlow<RegisterListUiState>(RegisterListUiState.Loading)
     val uiState: StateFlow<RegisterListUiState>
         get() = _uiState.asStateFlow()
+
+    fun onRegister(name:String, surname:String, email:String, password:String) {
+        viewModelScope.launch {
+            _uiState.value = RegisterListUiState.Loading
+
+            val result = userRepository.register(name, surname, email, password)
+            if (result.isSuccess) {
+                _uiState.value = RegisterListUiState.Registered
+            } else {
+                result.exceptionOrNull()?.let {
+                    _uiState.value = RegisterListUiState.Error(it.toString())
+                }
+            }
+        }
+    }
 }
 
 sealed class RegisterListUiState {
     data object InitialState : RegisterListUiState()
     data object Loading : RegisterListUiState()
-    class Error(message:String) : RegisterListUiState()
+    data class Error(val message:String) : RegisterListUiState()
     data object Registered : RegisterListUiState()
 }
 
